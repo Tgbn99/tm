6d5dae2b09f46bbcd9f1288de9166c82cfc10e27
Tgbn99 <telmo.nunes@live.com.pt>
2025-07-12 22:35:20 +0100

    TM-4 created subcategory model

---
+++ src/models/subcategoryModel.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const SubcategorySchema = new Schema(
+    {
+        subcategoryID: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        description: {
+            type: String,
+            maxlength: 100
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: "Category",
+            required: true
+        }
+    }
+)
+
+export default model("Subcategory", SubcategorySchema)
